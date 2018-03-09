e88a42721df86cac3670603ef430d8cf76fd2cdc
Harrison Balogh <harrisonbalogh@yahoo.com>
2018-03-09 04:31:11 -0800

    Encryption Library Added
    • Login2 is now called.

--- javascript/admin.js
+++ javascript/admin.js
@@ -2,15 +2,22 @@
 
 var textFieldPassword = document.getElementById("password-input");
 
-function submit() {
+function login1() {
   var httpRequest = new XMLHttpRequest();
 
   httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
        if (httpRequest.status == 200) {
          var response = JSON.parse(httpRequest.responseText);
-           console.log(response.salt);
+           console.log(response.salt); // concat to pass before hmac sha 256
+           console.log(response.challenge); // key for hmac
            console.log(textFieldPassword.value);
+           var shaObj = new jsSHA)"SHA-256", "TEXT");
+           shaObj.setHMACKey(response.challenge, "TEXT");
+           shaObj.update(textFieldPassword + "" + response.salt);
+           var hmac = shaObj.getHMAC("HEX");
+           console.log(hmac);
+           login2(hmac);
        }
        else if (httpRequest.status == 400) {
           alert('An error occurred: 400');
@@ -27,12 +34,36 @@ function submit() {
   httpRequest.send(JSON.stringify({ name: "hb"}));
 };
 
+function login2(postTag) {
+  var httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+       if (httpRequest.status == 200) {
+         var response = JSON.parse(httpRequest.responseText);
+         console.log(response);
+       }
+       else if (httpRequest.status == 400) {
+          alert('An error occurred: 400');
+       }
+       else {
+         alert('An unknown error occurred.');
+       }
+    }
+  };
+
+  httpRequest.open('POST', 'https://www.harxer.com/api/login2/');
+  httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  // httpRequest.send("{\"name\": \"hb\"}");
+  httpRequest.send(JSON.stringify({ name: "hb", tag: postTag}));
+};
+
 document.onkeypress = function(e) {
 	console.log(e);
 	e = e || window.event;
 	console.log("    or " + e);
 	if (e.code == "Enter" || e.key == "Enter" || e.keyCode  == 13 || e.charCode == 13) { // charCode is for firefox
     // var pass = textFieldPassword.value;
-    submit();
+    login1();
 	}
 };
